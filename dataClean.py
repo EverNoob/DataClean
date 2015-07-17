fb2725ffacb55c3e23db39e6cf1a4bb40ddf1fe1
Daniel Rowland <daniel.o.rowland@gmail.com>
2015-07-16 17:19:06 -0700

    Figuring out region stuff

--- dataClean.py
+++ dataClean.py
@@ -8,6 +8,19 @@ import datetime as dt
 __libchange__ = False
 
 size_dict = {'B': '750ml', 'E': '375ml', 'A': '750ml'}
+region_spelling_dict = {'MIDATLANTI': 'Mid Atlantic',
+                        'NEW ENGLAN': 'New England',
+                        'TRI-STATES': 'Tri-States',
+                        'CENTRAL': 'Central',
+                        'OH VALLEY': 'Ohio Valley',
+                        'Texas': 'Texas Region',
+                        'CAROLINAS': 'Carolinas',
+                        'FLORIDA': 'Florida',
+                        'GULF STATE': 'Gulf States',
+                        'MOUNTAIN': 'Mountain',
+                        'SOUTHWEST': 'Southwest'
+                        }
+innovation_east = ['Mid Atlantic', 'New England', 'Tri-States', 'Carolinas', 'Florida', 'Gulf States']
 
 
 def remove_vintage(n):
@@ -134,15 +147,16 @@ def build_brand_lookup():
 
     wb = openpyxl.load_workbook('Lookup Tables/2014 and YTD 2015 Shipment Data File.xlsx')
     ws = wb.get_sheet_by_name('Sheet1')
+    #the dictionary that yields a dictionary of brand associations given a
     brand_dict = {}
-    state_dict = {}
+    distributor_state_dict = {}
 
     for rownum in range(2, ws.get_highest_row()+1):
         brand_dict[ws.cell(row=rownum, column=1).value] = {'Brand Code': ws.cell(row=rownum, column=2).value,
                                                            'Brand': ws.cell(row=rownum, column=3).value,
                                                            'Varietal Code': ws.cell(row=rownum, column=4).value,
                                                            'Varietal': ws.cell(row=rownum, column=5).value,
-                                                           'SKU TAG': ws.cell(row=rownum, column=11),
+                                                           'SKU TAG': ws.cell(row=rownum, column=11).value,
                                                            'Portfolio': ws.cell(row=rownum, column=24).value,
                                                            'Category': ws.cell(row=rownum, column=25).value,
                                                            'Sales/Key Acct Rep': ws.cell(row=rownum, column=26).value,
@@ -150,40 +164,108 @@ def build_brand_lookup():
                                                            'IBM': ws.cell(row=rownum, column=28).value,
                                                            'SKU Cost': ws.cell(row=rownum, column=31).value
                                                            }
-        state_dict[ws.cell(row=rownum, column=6).value] = ws.cell(row=rownum, column=7).value
+        # 'Distributor Name':= 'state ID'                                              }
+        distributor_state_dict[ws.cell(row=rownum, column=6).value] = ws.cell(row=rownum, column=7).value
 
-    return brand_dict, state_dict
-
-
-
+    return brand_dict, distributor_state_dict
 
 
-def change_canada_region(obj_dict, state_dict):
-    sr = obj_dict['Sales Rep'].lower()
+#placeholder
+def get_region_from_state(state):
+    return "State"
+
+
+def refine_item_data(obj_dict, region_by_state_dict, state_by_distributor_dict, innovation_by_brand_dict):
+    region = obj_dict['Sales Rep'].lower()
     pf = obj_dict['Portfolio'].lower()
-    if 'canada' in sr:
+    distributor = obj_dict['Distributor']
+    skutag = obj_dict['SKU Tag'].lower()
+
+    #fix canada region
+    if 'canada' in region:
         obj_dict['State'] = 'Canada'
-        if sr[0] is 'e':
+        if region[0] is 'e':
             obj_dict['Sales Rep'] = 'East Canada'
         else:
             obj_dict['Sales Rep'] = 'West Canada'
-    if 'in' in sr:
+
+    #fix international region
+    if 'in' in region:
         obj_dict['Sales Rep'] = 'International'
         #cover the instance where there is a new distributor
-        if obj_dict['Distributor'] in state_dict:
-            obj_dict['State'] = state_dict[obj_dict['Distributor']]
-    if 'sales shipment' in obj_dict['Document Type'].lower() and ('core' in pf or 'v&e' in pf):
-        obj_dict['Sales Rep'] = 'Precept House'
+        if obj_dict['Distributor'] in state_by_distributor_dict:
+            obj_dict['State'] = state_by_distributor_dict[obj_dict['Distributor']]
+
+    #fix AA region
+    if 'alaska airlines' in obj_dict['Distributor'].lower():
+        obj_dict['Sales Rep'] = 'Airlines'
+
+    #fix precept house regions
+    #if the item is in the 'PH' region...
+    if 'ph' in region:
+        # and it is Core or V&E
+        if 'core' in pf or 'v&e' in pf:
+            # and it is a direct shipment account...
+            if 'sales shipment' in obj_dict['Document Type'].lower():
+                #make the region 'Precept House'
+                obj_dict['Sales Rep'] = 'Precept House'
+            elif obj_dict['Brand'] in innovation_by_brand_dict:
+                obj_dict['Sales Rep'] = 'Innovation'
+            else:
+                #otherwise make it the correct region according to the state
+                obj_dict['Sales Rep'] = region_by_state_dict[obj_dict['State']]
+
+    #fix TOTAL WINE region
+    if 'total wine' in region:
+        obj_dict['Sales Rep'] = 'Total Wine'
+
+    #fix Glazer's distributor title
+    if 'Glazer\'s of Texas'in distributor:
+        obj_dict['Distributor'] = 'Glazer\'s of Texas'
+
+    if 'Odom Corporation - Alaska' in distributor:
+        obj_dict['State'] = 'Alaska'
+        obj_dict['Sales Rep'] = "Mountain"
+    if 'Odom Corporation - Cour D\'Alen' in distributor or 'Odom Corporation - Lewiston' in distributor:
+        obj_dict['State'] = 'ID'
+        obj_dict['Sales Rep'] = 'NW WA'
+
+    if 'house wine box' in skutag:
+        obj_dict['Brand'] = 'House Wine Box'
+        obj_dict['Category'] = '3L BIB'
+    if 'house wind lone star' in skutag:
+        obj_dict['Brand'] = 'House Wine Lone Star'
+        obj_dict['Category'] = 'Innovation'
+    if 'ste chapelle box' in skutag:
+        obj_dict['Brand'] = 'Ste Chappelle Box'
+        obj_dict['Category'] = '3L BIB'
+    if 'wtso' in skutag:
+        obj_dict['Brand'] = 'WTSO'
+        obj_dict['Category'] = 'Innovation'
+    if 'grape & grain' in obj_dict['Portfolio'].lower():
+        if 'total wine' not in region and 'airlines' not in region and 'precept house' not in region:
+            if region in innovation_east:
+                obj_dict['Sales Rep'] = 'Innovation East'
+            else:
+                #obj_dict['Sales Rep'] = 'Innovation West'
+                a= 'beee'
+                #TODO this is fucking up because I haven't implemented region spelling changes yet. See step 17
+
 
     return obj_dict
 
 
 def generate_clean_data_list(rdl):
     scrubbed_data_list = []
-    brand_dict, state_dict = build_brand_lookup()
+    brand_dict = json.load(open('JSON Files/brandlookup.json', 'r'))
+    state_dict = json.load(open('JSON FIles/state_by_distributor.json', 'r'))
+    innovation_by_brand_dict = json.load(open('JSON Files/innovation_brands.json', 'r'))
+    state_by_distributor_dict = json.load(open('JSON Files/state_by_distributor.json', 'r'))
+    region_by_state_dict = json.load(open('JSON Files/regionbystatelookup_trimmed.json'))
 
     for raw_dict in rdl:
         clean_objdict = {}
+        #TODO add if statements to control what happens when itemcode is not in the brand_dict
         clean_objdict['Item code w/o vintage'] = itemcode = remove_vintage(raw_dict['Item No.'])
         clean_objdict['Brand Code'] = raw_dict['Brand Code']
         clean_objdict['Brand'] = brand_dict[itemcode]['Brand']
@@ -194,6 +276,7 @@ def generate_clean_data_list(rdl):
         clean_objdict['Sales Rep'] = raw_dict['Salesperson Code']
         clean_objdict['Item ID'] = raw_dict['Item No.']
         clean_objdict['Item'] = raw_dict['Description']
+        #TODO this SKU Tag assignment doesn't follow the formula, need to find out what that is.
         clean_objdict['SKU Tag'] = brand_dict[itemcode]['SKU Tag']
         clean_objdict['Item Pre'] = raw_dict['Item No.'][:8]
         clean_objdict['Size'] = itemcode[-1]
@@ -209,14 +292,17 @@ def generate_clean_data_list(rdl):
         clean_objdict['Vintage'] = raw_dict['Vintage']
         clean_objdict['Portfolio'] = brand_dict[itemcode]['Portfolio']
         clean_objdict['Category'] = brand_dict[itemcode]['Category']
-        clean_objdict['Sales/Key Acct Rep'] = brand_dict['Sales/Key Acct Rep']
-        clean_objdict['ISM'] = brand_dict['ISM']
-        clean_objdict['IBM'] = brand_dict['IBM']
+        clean_objdict['Sales/Key Acct Rep'] = brand_dict[itemcode]['Sales/Key Acct Rep']
+        clean_objdict['ISM'] = brand_dict[itemcode]['ISM']
+        clean_objdict['IBM'] = brand_dict[itemcode]['IBM']
         clean_objdict['Customer ID'] = raw_dict['Customer No.']
         clean_objdict['Sales FOB'] = raw_dict['Sales Amount (Actual)'] / raw_dict['Quantity (positive)']
-        clean_objdict['SKU Cost'] = 'PlaceHolder'
-
-
+        clean_objdict['SKU Cost'] = brand_dict[itemcode]['SKU Cost']
+        #final scrub
+        clean_objdict = refine_item_data(clean_objdict,
+                                         region_by_state_dict,
+                                         state_by_distributor_dict,
+                                         innovation_by_brand_dict)
         scrubbed_data_list.append(clean_objdict)
     return scrubbed_data_list
 
@@ -224,8 +310,8 @@ def generate_clean_data_list(rdl):
 def main():
 
     # Open the workbook and select the first worksheet
-    wb = xlrd.open_workbook('Input/Dirty Data.xlsx')
-    sh = wb.sheet_by_index(0)
+    # wb = xlrd.open_workbook('Input/Dirty Data.xlsx')
+    # sh = wb.sheet_by_index(0)
 
     wbtest = openpyxl.load_workbook('Input/Dirty Data.xlsx')
     shtest = wbtest.get_active_sheet()
@@ -234,18 +320,18 @@ def main():
 
     print ("the size of data_list: " + str(len(raw_data_list)))
     # Serialize the list of dicts to JSON
-    j = json.dumps(raw_data_list, sort_keys=True, indent=4 * ' ')
-    L = build_lookup_json()
+    #j = json.dumps(raw_data_list, sort_keys=True, indent=4 * ' ')
+    # L = build_lookup_json()
 
 
-    # Write to file
-    with open('rawdata.json', 'w') as f:
-        f.write(j)
-        f.close()
-
-    with open('lookup.json', 'w') as f:
-        f.write(L)
-        f.close()
+    # # Write to file
+    # with open('rawdata.json', 'w') as f:
+    #     f.write(j)
+    #     f.close()
+    #
+    # with open('lookup.json', 'w') as f:
+    #     f.write(L)
+    #     f.close()
 
     prettyprint(generate_clean_data_list(raw_data_list))
 
--- lookuptools.py
+++ lookuptools.py
@@ -9,15 +9,63 @@ def build_brand_lookup():
     ws = wb.get_sheet_by_name('Sheet1')
     brand_dict = {}
     for rownum in range(2, ws.get_highest_row()+1):
-        brand_dict[ws.cell(row=rownum, column=1).value] = {'Brand Code': ws.cell(row=rownum, column=2).value, 'Brand': ws.cell(row=rownum, column=3).value,
-                                               'Varietal Code': ws.cell(row=rownum, column=4).value, 'Varietal': ws.cell(row=rownum, column=5).value,
-                                               'Portfolio': ws.cell(row=rownum, column=24).value, 'Category': ws.cell(row=rownum, column=25).value}
+        brand_dict[ws.cell(row=rownum, column=1).value] = {'Brand Code': ws.cell(row=rownum, column=2).value,
+                                                           'Brand': ws.cell(row=rownum, column=3).value,
+                                                           'Varietal Code': ws.cell(row=rownum, column=4).value,
+                                                           'Varietal': ws.cell(row=rownum, column=5).value,
+                                                           'SKU Tag': ws.cell(row=rownum, column=11).value,
+                                                           'Portfolio': ws.cell(row=rownum, column=24).value,
+                                                           'Category': ws.cell(row=rownum, column=25).value,
+                                                           'Sales/Key Acct Rep': ws.cell(row=rownum, column=26).value,
+                                                           'ISM': ws.cell(row=rownum, column=27).value,
+                                                           'IBM': ws.cell(row=rownum, column=28).value,
+                                                           'SKU Cost': ws.cell(row=rownum, column=31).value
+                                                           }
     with open('JSON Files/brandlookup.json', 'w') as f:
-        f.write(json.dumps([brand_dict], sort_keys=True, indent=4 * ' '))
+        f.write(json.dumps(brand_dict, sort_keys=True, indent=4 * ' '))
+
+def build_innovation_lookup():
+    wb = openpyxl.load_workbook('Lookup Tables/2014 and YTD 2015 Shipment Data File.xlsx')
+    ws = wb.get_sheet_by_name('Sheet1')
+    innovation_dict = {}
+    for rownum in range(2, ws.get_highest_row()+1):
+        #look for whether the brand in a "core brand" and not record the brand/innovation relationship
+        if 'core' not in ws.cell(row=rownum, column=24).value.lower() and 'innovation' in ws.cell(row=rownum, column=8).value.lower():
+            innovation_dict[ws.cell(row=rownum, column=3).value] = ws.cell(row=rownum, column=8).value
+    with open('JSON Files/innovation_brands.json', 'w') as f:
+        f.write(json.dumps(innovation_dict, sort_keys=True, indent= 4 * ' '))
+
+def build_state_by_distributor_lookup():
+    wb = openpyxl.load_workbook('Lookup Tables/2014 and YTD 2015 Shipment Data File.xlsx')
+    ws = wb.get_sheet_by_name('Sheet1')
+    state_by_distributor_dict = {}
+    #TODO: account for the distributors that operate in multiple states,
+    #  perhaps by making each dist a ref. to a list
+    for rownum in range(2, ws.get_highest_row()+1):
+        state_by_distributor_dict[ws.cell(row=rownum, column=6).value] = ws.cell(row=rownum, column=7).value
+    with open('JSON Files/state_by_distributor.json', 'w') as f:
+        f.write(json.dumps(state_by_distributor_dict, sort_keys=True, indent= 4 * ' '))
+
+def build_region_by_state_lookup():
+    #TODO automate the process of eliminating all other regions, stub list below
+    regions_to_delete = ['Airlines', 'California Region']
+    wb = openpyxl.load_workbook('Lookup Tables/2014 and YTD 2015 Shipment Data File.xlsx')
+    ws = wb.get_sheet_by_name('Sheet1')
+    region_by_state_dict = {}
+    for rownum in range(2, ws.get_highest_row()+1):
+        region = ws.cell(row=rownum, column=8).value
+        state = ws.cell(row=rownum, column=7).value
+        if region in region_by_state_dict:
+            if state not in region_by_state_dict[region]:
+                region_by_state_dict[region].append(state)
+        else:
+            region_by_state_dict[region] = [state]
+    return region_by_state_dict
+    with open('JSON Files/regionbystatelookup.json', 'w') as f:
+        f.write(json.dumps(region_by_state_dict, sort_keys=True, indent=4 * ' '))
 
 
 
 build_brand_lookup()
-
 
 
